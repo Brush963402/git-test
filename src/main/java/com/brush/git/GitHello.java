e91598c4aba15b7dd694e7f97e7991af17866c76
brush <brush@163.com>
2022-03-29 15:30:27 +0800

    second update

--- src/main/java/com/brush/git/GitHello.java
+++ src/main/java/com/brush/git/GitHello.java
@@ -3,5 +3,7 @@ package com.brush.git;
 public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
